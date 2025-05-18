7ffe3a2e40a53d9ad0087bb1dfa7158366114ce1
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-05-18 18:29:07 -0500

    adjust logo

--- TownOfUs/Patches/Misc/LogoPatch.cs
+++ TownOfUs/Patches/Misc/LogoPatch.cs
@@ -23,7 +23,7 @@ public static class LogoPatch
         }
         if (sizer != null)
         {
-            sizer.GetComponent<AspectSize>().PercentWidth = 0.33f;
+            sizer.GetComponent<AspectSize>().PercentWidth = 0.32f;
         }
     }
 }
