03d609b727851b7e4a4613c3fe11c2789bf873aa
Zeo666 <zeo66682@gmail.com>
2025-05-28 13:59:25 +0100

    Removed custom player positioning for lovers game over screen

--- TownOfUs/GameOver/LoverGameOver.cs
+++ TownOfUs/GameOver/LoverGameOver.cs
@@ -18,26 +18,26 @@ public sealed class LoverGameOver : CustomGameOver
 
     public override void AfterEndGameSetup(EndGameManager endGameManager)
     {
-        PoolablePlayer[] array = Object.FindObjectsOfType<PoolablePlayer>();
-        if (array[0] != null)
-        {
-            array[0].SetFlipX(true);
+        //PoolablePlayer[] array = Object.FindObjectsOfType<PoolablePlayer>();
+        //if (array[0] != null)
+        //{
+        //    array[0].SetFlipX(true);
 
-            array[0].gameObject.transform.position -= new Vector3(1.5f, 0f, 0f);
-            array[0].cosmetics.skin.transform.localScale = new Vector3(-1, 1, 1);
-            array[0].cosmetics.nameText.color = new Color(1f, 0.4f, 0.8f, 1f);
-        }
+        //    array[0].gameObject.transform.position -= new Vector3(1.5f, 0f, 0f);
+        //    array[0].cosmetics.skin.transform.localScale = new Vector3(-1, 1, 1);
+        //    array[0].cosmetics.nameText.color = new Color(1f, 0.4f, 0.8f, 1f);
+        //}
 
-        if (array[1] != null)
-        {
-            array[1].SetFlipX(false);
+        //if (array[1] != null)
+        //{
+        //    array[1].SetFlipX(false);
 
-            array[1].gameObject.transform.position = array[0].gameObject.transform.position + new Vector3(1.2f, 0f, 0f);
-            array[1].gameObject.transform.localScale *= 0.92f;
-            array[1].cosmetics.skin.transform.localScale = new Vector3(-1, 1, 1);
-            array[1].cosmetics.hat.transform.position += new Vector3(0.1f, 0f, 0f);
-            array[1].cosmetics.nameText.color = new Color(1f, 0.4f, 0.8f, 1f);
-        }
+        //    array[1].gameObject.transform.position = array[0].gameObject.transform.position + new Vector3(1.2f, 0f, 0f);
+        //    array[1].gameObject.transform.localScale *= 0.92f;
+        //    array[1].cosmetics.skin.transform.localScale = new Vector3(-1, 1, 1);
+        //    array[1].cosmetics.hat.transform.position += new Vector3(0.1f, 0f, 0f);
+        //    array[1].cosmetics.nameText.color = new Color(1f, 0.4f, 0.8f, 1f);
+        //}
 
         endGameManager.BackgroundBar.material.SetColor(ShaderID.Color, TownOfUsColors.Lover);
 
