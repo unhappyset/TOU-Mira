fbc0178a7cca317f93ce23f42bac0fbbcd86fee6
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-05-29 19:49:15 -0500

    Fix Disabled Modifier checks

--- TownOfUs/Buttons/Impostor/MorphlingMorphButton.cs
+++ TownOfUs/Buttons/Impostor/MorphlingMorphButton.cs
@@ -23,7 +23,7 @@ public sealed class MorphlingMorphButton : TownOfUsRoleButton<MorphlingRole>, IA
 
     public override bool CanUse()
     {
-        return ((Timer <= 0 && !EffectActive) || (EffectActive && Timer <= (EffectDuration - 2f))) && !PlayerControl.LocalPlayer.HasModifier<GlitchHackedModifier>() || PlayerControl.LocalPlayer.HasModifier<DisabledModifier>();
+        return ((Timer <= 0 && !EffectActive) || (EffectActive && Timer <= (EffectDuration - 2f))) && !PlayerControl.LocalPlayer.HasModifier<GlitchHackedModifier>() && !PlayerControl.LocalPlayer.HasModifier<DisabledModifier>();
     }
 
     public override void ClickHandler()
