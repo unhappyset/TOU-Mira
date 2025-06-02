540fca7a03d31d5d74e4ea529f72076a7430ed9d
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-06-02 09:53:51 -0500

    Add more freeplay improvements

---
+++ TownOfUs/Patches/DummyBehaviourPatch.cs
@@ -0,0 +1,30 @@
+using HarmonyLib;
+using MiraAPI.Roles;
+using Reactor.Utilities.Extensions;
+using TownOfUs.Roles.Impostor;
+using TownOfUs.Roles.Neutral;
+using TownOfUs.Utilities;
+
+namespace TownOfUs.Patches;
+
+[HarmonyPatch(typeof(DummyBehaviour))]
+public static class DummyBehaviourPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DummyBehaviour.Start))]
+    public static void DummyStartPatch(DummyBehaviour __instance)
+    {
+        var roleList = MiscUtils.AllRoles
+            .Where(role => role is ICustomRole)
+            .Where(role => role is not TraitorRole)
+            .Where(role => role is not NeutralGhostRole)
+            .Where(role => role is not CrewmateGhostRole)
+            .Where(role => role is not ImpostorGhostRole)
+            .ToList();
+
+        var roleType = RoleId.Get(roleList.Random()!.GetType());
+        __instance.myPlayer.RpcChangeRole(roleType, false);
+
+        __instance.myPlayer.RpcSetName(AccountManager.Instance.GetRandomName());
+    }
+}
