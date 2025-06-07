bb5b29e41ea8e636c16f1e848f647c255d9852bb
whichtwix <whichtwix@users.noreply.github.com>
2025-06-07 18:47:24 +0300

    clear some stuff with 0 references

--- TownOfUs/Modules/Rainbow/Utils.cs
+++ TownOfUs/Modules/Rainbow/Utils.cs
@@ -1,22 +1,12 @@
+using MiraAPI.Utilities;
 using UnityEngine;
 
 namespace TownOfUs.Modules.RainbowMod;
 
 public static class RainbowUtils
 {
-    private static readonly int BackColor = Shader.PropertyToID("_BackColor");
-    private static readonly int BodyColor = Shader.PropertyToID("_BodyColor");
-    private static readonly int VisorColor = Shader.PropertyToID("_VisorColor");
-
-
     public static Color Rainbow => new HSBColor(PP(0, 1, 0.3f), 1, 1).ToColor();
     public static Color RainbowShadow => Shadow(Rainbow);
-
-    public static Color Galaxy => new HSBColor(PP(0.5f, 0.87f, 0.4f), 1, 1).ToColor();
-    public static Color GalaxyShadow => Shadow(Galaxy);
-
-    public static Color Fire => new HSBColor(PP(0f, 0.17f, 0.4f), 1, 1).ToColor();
-    public static Color FireShadow => Shadow(Fire);
 
     public static float PP(float min, float max, float mul)
     {
@@ -30,9 +20,9 @@ public static class RainbowUtils
 
     public static void SetRainbow(Renderer rend)
     {
-        rend.material.SetColor(BackColor, RainbowShadow);
-        rend.material.SetColor(BodyColor, Rainbow);
-        rend.material.SetColor(VisorColor, Palette.VisorColor);
+        rend.material.SetColor(ShaderID.BackColor, RainbowShadow);
+        rend.material.SetColor(ShaderID.BodyColor, Rainbow);
+        rend.material.SetColor(ShaderID.VisorColor, Palette.VisorColor);
     }
 
     public static bool IsRainbow(int id)
@@ -55,83 +45,12 @@ public struct HSBColor
     public float b;
     public float a;
 
-    public HSBColor(float h, float s, float b, float a)
-    {
-        this.h = h;
-        this.s = s;
-        this.b = b;
-        this.a = a;
-    }
-
     public HSBColor(float h, float s, float b)
     {
         this.h = h;
         this.s = s;
         this.b = b;
         a = 1f;
-    }
-
-    public HSBColor(Color col)
-    {
-        var temp = FromColor(col);
-        h = temp.h;
-        s = temp.s;
-        b = temp.b;
-        a = temp.a;
-    }
-
-    public static HSBColor FromColor(Color color)
-    {
-        var ret = new HSBColor(0f, 0f, 0f, color.a);
-
-        var r = color.r;
-        var g = color.g;
-        var b = color.b;
-
-        var max = Mathf.Max(r, Mathf.Max(g, b));
-
-        if (max <= 0)
-        {
-            return ret;
-        }
-
-        var min = Mathf.Min(r, Mathf.Min(g, b));
-        var dif = max - min;
-
-        if (max > min)
-        {
-            if (g == max)
-            {
-                ret.h = (b - r) / dif * 60f + 120f;
-            }
-            else if (b == max)
-            {
-                ret.h = (r - g) / dif * 60f + 240f;
-            }
-            else if (b > g)
-            {
-                ret.h = (g - b) / dif * 60f + 360f;
-            }
-            else
-            {
-                ret.h = (g - b) / dif * 60f;
-            }
-
-            if (ret.h < 0)
-            {
-                ret.h = ret.h + 360f;
-            }
-        }
-        else
-        {
-            ret.h = 0;
-        }
-
-        ret.h *= 1f / 360f;
-        ret.s = (dif / max) * 1f;
-        ret.b = max;
-
-        return ret;
     }
 
     public static Color ToColor(HSBColor hsbColor)
@@ -194,55 +113,8 @@ public struct HSBColor
         return new Color(Mathf.Clamp01(r), Mathf.Clamp01(g), Mathf.Clamp01(b), hsbColor.a);
     }
 
-    public Color ToColor()
+    public readonly Color ToColor()
     {
         return ToColor(this);
-    }
-
-    public override string ToString()
-    {
-        return "H:" + h + " S:" + s + " B:" + b;
-    }
-
-    public static HSBColor Lerp(HSBColor a, HSBColor b, float t)
-    {
-        float h, s;
-
-        //check special case black (color.b==0): interpolate neither hue nor saturation!
-        //check special case grey (color.s==0): don't interpolate hue!
-        if (a.b == 0)
-        {
-            h = b.h;
-            s = b.s;
-        }
-        else if (b.b == 0)
-        {
-            h = a.h;
-            s = a.s;
-        }
-        else
-        {
-            if (a.s == 0)
-            {
-                h = b.h;
-            }
-            else if (b.s == 0)
-            {
-                h = a.h;
-            }
-            else
-            {
-                var angle = Mathf.LerpAngle(a.h * 360f, b.h * 360f, t);
-                while (angle < 0f)
-                    angle += 360f;
-                while (angle > 360f)
-                    angle -= 360f;
-                h = angle / 360f;
-            }
-
-            s = Mathf.Lerp(a.s, b.s, t);
-        }
-
-        return new HSBColor(h, s, Mathf.Lerp(a.b, b.b, t), Mathf.Lerp(a.a, b.a, t));
     }
 }
