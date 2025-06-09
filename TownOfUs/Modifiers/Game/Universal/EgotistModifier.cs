1d0af5f4bde6a818214a35432470e93a0daacf71
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-06-08 23:02:47 -0500

    Rewrite Egotist text

--- TownOfUs/Modifiers/Game/Universal/EgotistModifier.cs
+++ TownOfUs/Modifiers/Game/Universal/EgotistModifier.cs
@@ -14,9 +14,9 @@ public sealed class EgotistModifier : AllianceGameModifier, IWikiDiscoverable
     public override string ModifierName => "Egotist";
     public override string IntroInfo => $"Your Ego is Thriving...";
     public override string Symbol => $"<size=100%>#</size>";
-    public override float IntroSize => 4.4f;
+    public override float IntroSize => 4f;
     public override LoadableAsset<Sprite>? ModifierIcon => TouModifierIcons.Egotist;
-    public override string GetDescription() => "Defy the crew,\nwin with killers.";
+    public override string GetDescription() => "<size=130%>Defy the crew,</size>\n<size=125%>win with killers.</size>";
     public override int GetAssignmentChance() => (int)OptionGroupSingleton<AllianceModifierOptions>.Instance.EgotistChance;
     public override int GetAmountPerGame() => 1;
     public int Priority { get; set; } = 5;
@@ -24,7 +24,7 @@ public sealed class EgotistModifier : AllianceGameModifier, IWikiDiscoverable
 
     public string GetAdvancedDescription()
     {
-        return $"The Egotist is an Alliance modifier that only applies to crewmates (signified with <color=#669966> #</color>). As the Egotist, you can only win if Impostors or Neutral Killers win. It does not matter if you are alive or not, but if no crewmates remain after a meeting ends, you will leave in victory. You also do not get punished for harming the crewmates";
+        return $"The Egotist is a Crewmate Alliance modifier (signified by <color=#669966>#</color>). As the Egotist, you can only win if Impostors or Neutral Killers win. If no crewmates remain after a meeting ends, you will leave in victory.";
     }
 
     public override bool IsModifierValidOn(RoleBehaviour role)
