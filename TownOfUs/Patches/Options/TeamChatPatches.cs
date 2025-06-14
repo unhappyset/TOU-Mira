f19478eeab94ed084fc8f13df1d75bce72d70d1f
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-06-14 15:30:20 -0500

    Team Chat Closes Reliably now

--- TownOfUs/Patches/Options/TeamChatPatches.cs
+++ TownOfUs/Patches/Options/TeamChatPatches.cs
@@ -93,63 +93,80 @@ public static class TeamChatPatches
     {
         public static void Postfix(ChatController __instance)
         {
-            if (__instance.IsOpenOrOpening)
+            try
             {
-                if (_teamText == null)
+                if (__instance.IsOpenOrOpening)
                 {
-                    _teamText = Object.Instantiate(__instance.sendRateMessageText, __instance.sendRateMessageText.transform.parent);
-                    _teamText.text = string.Empty;
-                    _teamText.color = TownOfUsColors.ImpSoft;
-                }
-                _teamText.text = string.Empty;
-                var ChatScreenContainer = GameObject.Find("ChatScreenContainer");
-                // var FreeChat = GameObject.Find("FreeChatInputField");
-                var Background = ChatScreenContainer.transform.FindChild("Background");
-                // var bubbleItems = GameObject.Find("Items");
-                // var typeBg = FreeChat.transform.FindChild("Background");
-                // var typeText = FreeChat.transform.FindChild("Text");
-
-                if (TeamChatActive)
-                {
-                    var ogChat = HudManager.Instance.Chat.chatButton;
-                    ogChat.transform.Find("Inactive").gameObject.SetActive(true);
-                    ogChat.transform.Find("Active").gameObject.SetActive(false);
-                    ogChat.transform.Find("Selected").gameObject.SetActive(false);
-
-                    var genOpt = OptionGroupSingleton<GeneralOptions>.Instance;
-                    Background.GetComponent<SpriteRenderer>().color = new Color(0.2f, 0.1f, 0.1f, 0.8f);
-                    //typeBg.GetComponent<SpriteRenderer>().color = new Color(0.2f, 0.1f, 0.1f, 0.6f);
-                    //typeText.GetComponent<TextMeshPro>().color = Color.white;
-                    if (MeetingHud.Instance)
+                    if (_teamText == null)
                     {
-                        ChatScreenContainer.transform.localPosition = HudManager.Instance.Chat.chatButton.transform.localPosition - new Vector3(3.5133f + 4.33f * (Camera.main.orthographicSize / 3f), 4.576f);
+                        _teamText = Object.Instantiate(__instance.sendRateMessageText, __instance.sendRateMessageText.transform.parent);
+                        _teamText.text = string.Empty;
+                        _teamText.color = TownOfUsColors.ImpSoft;
+                    }
+                    _teamText.text = string.Empty;
+                    var ChatScreenContainer = GameObject.Find("ChatScreenContainer");
+                    // var FreeChat = GameObject.Find("FreeChatInputField");
+                    var Background = ChatScreenContainer.transform.FindChild("Background");
+                    // var bubbleItems = GameObject.Find("Items");
+                    // var typeBg = FreeChat.transform.FindChild("Background");
+                    // var typeText = FreeChat.transform.FindChild("Text");
+
+                    if (TeamChatActive)
+                    {
+                        var ogChat = HudManager.Instance.Chat.chatButton;
+                        ogChat.transform.Find("Inactive").gameObject.SetActive(true);
+                        ogChat.transform.Find("Active").gameObject.SetActive(false);
+                        ogChat.transform.Find("Selected").gameObject.SetActive(false);
+
+                        var genOpt = OptionGroupSingleton<GeneralOptions>.Instance;
+                        Background.GetComponent<SpriteRenderer>().color = new Color(0.2f, 0.1f, 0.1f, 0.8f);
+                        //typeBg.GetComponent<SpriteRenderer>().color = new Color(0.2f, 0.1f, 0.1f, 0.6f);
+                        //typeText.GetComponent<TextMeshPro>().color = Color.white;
+                        if (MeetingHud.Instance)
+                        {
+                            ChatScreenContainer.transform.localPosition = HudManager.Instance.Chat.chatButton.transform.localPosition - new Vector3(3.5133f + 4.33f * (Camera.main.orthographicSize / 3f), 4.576f);
+                        }
+                        else
+                        {
+                            ChatScreenContainer.transform.localPosition = HudManager.Instance.Chat.chatButton.transform.localPosition - new Vector3(3.5133f + 3.49f * (Camera.main.orthographicSize / 3f), 4.576f);
+                        }
+
+                        if ((PlayerControl.LocalPlayer.IsJailed() || PlayerControl.LocalPlayer.Data.Role is JailorRole) && _teamText != null)
+                        {
+                            _teamText.text = "Jailor Chat is Open. Only the Jailor and Jailee can see this.";
+                            _teamText.color = TownOfUsColors.Jailor;
+                        }
+                        else if (PlayerControl.LocalPlayer.IsImpostor() && genOpt is { FFAImpostorMode: false, ImpostorChat.Value: true } && !PlayerControl.LocalPlayer.Data.IsDead && _teamText != null)
+                        {
+                            _teamText.text = "Impostor Chat is Open. Only Impostors can see this.";
+                            _teamText.color = TownOfUsColors.ImpSoft;
+                        }
+                        else if (PlayerControl.LocalPlayer.Data.Role is VampireRole && genOpt.VampireChat && !PlayerControl.LocalPlayer.Data.IsDead && _teamText != null)
+                        {
+                            _teamText.text = "Vampire Chat is Open. Only Vampires can see this.";
+                            _teamText.color = TownOfUsColors.Vampire;
+                        }
+                        else if (_teamText != null)
+                        {
+                            _teamText.text = "Jailor, Impostor, and Vampire Chat can be seen here.";
+                            _teamText.color = Color.white;
+                        }
+                        /* foreach (var bubble in bubbleItems.GetAllChilds())
+                            {
+                                bubble.gameObject.SetActive(true);
+                                var bg = bubble.transform.Find("Background").gameObject;
+                                if (bg != null)
+                                {
+                                    var sprite = bg.GetComponent<SpriteRenderer>();
+                                    var color = sprite.color.SetAlpha(1f);
+                                    if (color == Color.white || color == Color.black) bubble.gameObject.SetActive(false);
+                                }
+                            }
+                        __instance.AlignAllBubbles(); */
                     }
                     else
                     {
-                        ChatScreenContainer.transform.localPosition = HudManager.Instance.Chat.chatButton.transform.localPosition - new Vector3(3.5133f + 3.49f * (Camera.main.orthographicSize / 3f), 4.576f);
-                    }
-
-                    if ((PlayerControl.LocalPlayer.IsJailed() || PlayerControl.LocalPlayer.Data.Role is JailorRole) && _teamText != null)
-                    {
-                        _teamText.text = "Jailor Chat is Open. Only the Jailor and Jailee can see this.";
-                        _teamText.color = TownOfUsColors.Jailor;
-                    }
-                    else if (PlayerControl.LocalPlayer.IsImpostor() && genOpt is { FFAImpostorMode: false, ImpostorChat.Value: true } && !PlayerControl.LocalPlayer.Data.IsDead && _teamText != null)
-                    {
-                        _teamText.text = "Impostor Chat is Open. Only Impostors can see this.";
-                        _teamText.color = TownOfUsColors.ImpSoft;
-                    }
-                    else if (PlayerControl.LocalPlayer.Data.Role is VampireRole && genOpt.VampireChat && !PlayerControl.LocalPlayer.Data.IsDead && _teamText != null)
-                    {
-                        _teamText.text = "Vampire Chat is Open. Only Vampires can see this.";
-                        _teamText.color = TownOfUsColors.Vampire;
-                    }
-                    else if (_teamText != null)
-                    {
-                        _teamText.text = "Jailor, Impostor, and Vampire Chat can be seen here.";
-                        _teamText.color = Color.white;
-                    }
-                    /* foreach (var bubble in bubbleItems.GetAllChilds())
+                        /* foreach (var bubble in bubbleItems.GetAllChilds())
                         {
                             bubble.gameObject.SetActive(true);
                             var bg = bubble.transform.Find("Background").gameObject;
@@ -157,40 +174,30 @@ public static class TeamChatPatches
                             {
                                 var sprite = bg.GetComponent<SpriteRenderer>();
                                 var color = sprite.color.SetAlpha(1f);
-                                if (color == Color.white || color == Color.black) bubble.gameObject.SetActive(false);
+                                if (color != Color.white && color != Color.black) bubble.gameObject.SetActive(false);
                             }
-                        }
-                    __instance.AlignAllBubbles(); */
-                }
-                else
-                {
-                    /* foreach (var bubble in bubbleItems.GetAllChilds())
-                    {
-                        bubble.gameObject.SetActive(true);
-                        var bg = bubble.transform.Find("Background").gameObject;
-                        if (bg != null)
+                        } */
+                        Background.GetComponent<SpriteRenderer>().color = Color.white;
+                        /* typeBg.GetComponent<SpriteRenderer>().color = Color.white;
+                        typeBg.GetComponent<ButtonRolloverHandler>().ChangeOutColor(Color.white);
+                        typeBg.GetComponent<ButtonRolloverHandler>().OverColor = new Color(0f, 1f, 0f, 1f);
+                        if (typeText.TryGetComponent<TextMeshPro>(out var txt))
                         {
-                            var sprite = bg.GetComponent<SpriteRenderer>();
-                            var color = sprite.color.SetAlpha(1f);
-                            if (color != Color.white && color != Color.black) bubble.gameObject.SetActive(false);
+                            txt.color = new Color(0.6706f, 0.8902f, 0.8667f, 1f);
+                            txt.SetFaceColor(new Color(0.6706f, 0.8902f, 0.8667f, 1f));
                         }
-                    } */
-                    Background.GetComponent<SpriteRenderer>().color = Color.white;
-                    /* typeBg.GetComponent<SpriteRenderer>().color = Color.white;
-                    typeBg.GetComponent<ButtonRolloverHandler>().ChangeOutColor(Color.white);
-                    typeBg.GetComponent<ButtonRolloverHandler>().OverColor = new Color(0f, 1f, 0f, 1f);
-                    if (typeText.TryGetComponent<TextMeshPro>(out var txt))
-                    {
-                        txt.color = new Color(0.6706f, 0.8902f, 0.8667f, 1f);
-                        txt.SetFaceColor(new Color(0.6706f, 0.8902f, 0.8667f, 1f));
+                        typeText.GetComponent<TextMeshPro>().color = new Color(0.6706f, 0.8902f, 0.8667f, 1f); */
+                        ChatScreenContainer.transform.localPosition = HudManager.Instance.Chat.chatButton.transform.localPosition - new Vector3(3.5133f + 3.49f * (Camera.main.orthographicSize / 3f), 4.576f);
                     }
-                    typeText.GetComponent<TextMeshPro>().color = new Color(0.6706f, 0.8902f, 0.8667f, 1f); */
-                    ChatScreenContainer.transform.localPosition = HudManager.Instance.Chat.chatButton.transform.localPosition - new Vector3(3.5133f + 3.49f * (Camera.main.orthographicSize / 3f), 4.576f);
+                }
+                else if (TeamChatActive)
+                {
+                    ToggleTeamChat();
                 }
             }
-            else if (__instance.IsClosedOrClosing && TeamChatActive)
+            catch
             {
-                ToggleTeamChat();
+                // Nothing Happens Here
             }
         }
     }
