040cb274400bf1a55d77c2215a738bb227c23423
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-06-16 19:54:02 -0500

    Fix Detective Inspect Button

--- TownOfUs/Buttons/Crewmate/DetectiveInspectButton.cs
+++ TownOfUs/Buttons/Crewmate/DetectiveInspectButton.cs
@@ -32,6 +32,6 @@ public sealed class DetectiveInspectButton : TownOfUsRoleButton<DetectiveRole, C
         Role.InvestigatedPlayers.AddRange(Target.GetScenePlayers());
         var notif1 = Helpers.CreateAndShowNotification($"<b>{TownOfUsColors.Detective.ToTextColor()}You have inspected the crime scene of {Target.DeadPlayer!.Data.PlayerName}. The killer or anyone that steps foot in the crime scene will flash red when examined.</b></color>", Color.white, new Vector3(0f, 1f, -20f), spr: TouRoleIcons.Detective.LoadAsset());
         notif1.Text.SetOutlineThickness(0.35f);
-        TouAudio.PlaySound(TouAudio.QuestionSound);
+        // TouAudio.PlaySound(TouAudio.QuestionSound);
     }
 }
