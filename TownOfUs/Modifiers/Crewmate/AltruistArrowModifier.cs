a61273b88b948b451a515b4f1d18488ef8974014
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-07-04 09:23:25 -0500

    Fix altruist arrows on death

--- TownOfUs/Modifiers/Crewmate/AltruistArrowModifier.cs
+++ TownOfUs/Modifiers/Crewmate/AltruistArrowModifier.cs
@@ -5,4 +5,9 @@ namespace TownOfUs.Modifiers.Crewmate;
 public sealed class AltruistArrowModifier(PlayerControl owner, Color color) : ArrowTargetModifier(owner, color, 0)
 {
     public override string ModifierName => "Altruist Arrow";
+
+    public override void OnDeath(DeathReason reason)
+    {
+        ModifierComponent?.RemoveModifier(this);
+    }
 }
