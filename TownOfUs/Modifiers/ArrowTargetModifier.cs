892b46af894bd3dab1c19e936b7121170895abc9
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-07-04 20:43:17 -0500

    Fix rainbow tracker arrows

--- TownOfUs/Modifiers/ArrowTargetModifier.cs
+++ TownOfUs/Modifiers/ArrowTargetModifier.cs
@@ -13,6 +13,7 @@ public abstract class ArrowTargetModifier(PlayerControl owner, Color color, floa
 
     private ArrowBehaviour? _arrow;
     private DateTime _time = DateTime.UnixEpoch;
+    public ArrowBehaviour? Arrow => _arrow;
     public override string ModifierName => "Arrow Target";
     public override bool Unique => false;
     public override bool HideOnUi => true;
