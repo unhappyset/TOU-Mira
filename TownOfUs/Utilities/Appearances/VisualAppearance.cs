6cb0d7c9b524ae26677f6cbe4ad96a0efe618c9c
MyDragonBreath <38029594+MyDragonBreath@users.noreply.github.com>
2025-07-10 19:20:39 +1000

    Visor Color Appearances Api Update

--- TownOfUs/Utilities/Appearances/VisualAppearance.cs
+++ TownOfUs/Utilities/Appearances/VisualAppearance.cs
@@ -32,6 +32,7 @@ public sealed class VisualAppearance : NetworkedPlayerInfo.PlayerOutfit
 
         RendererColor = outfit.RendererColor;
         PlayerMaterialColor = outfit.PlayerMaterialColor;
+        PlayerMaterialVisorColor = outfit.PlayerMaterialVisorColor;
         NameColor = outfit.NameColor;
         ColorBlindTextColor = outfit.ColorBlindTextColor;
 
@@ -44,13 +45,10 @@ public sealed class VisualAppearance : NetworkedPlayerInfo.PlayerOutfit
 
     public Color RendererColor { get; set; } = Color.white;
     public Color? PlayerMaterialColor { get; set; }
+    public Color? PlayerMaterialVisorColor { get; set; }
     public Color? NameColor { get; set; }
     public Color ColorBlindTextColor { get; set; } = Color.white;
     public bool NameVisible { get; set; } = true;
-
-    // CODE REVIEW 22/2/2025 AEDT (D/M/Y)
-    // ---------------------------------
-    // I should do this
 
     public float Speed { get; set; } = 1f;
     public Vector3 Size { get; set; } = new(0.7f, 0.7f, 1f);
