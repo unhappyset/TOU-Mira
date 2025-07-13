569714477d633edb755e427bade127a1c515582b
whichtwix <whichtwix@users.noreply.github.com>
2025-07-13 13:57:14 +0600

    remove hypno mod on hypnotist death/disconnect

--- TownOfUs/Events/Impostor/HypnotistEvents.cs
+++ TownOfUs/Events/Impostor/HypnotistEvents.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using MiraAPI.Events;
 using MiraAPI.Events.Vanilla.Meeting;
+using MiraAPI.Events.Vanilla.Player;
 using MiraAPI.Modifiers;
 using MiraAPI.Roles;
 using TownOfUs.Modifiers.Impostor;
@@ -33,4 +34,25 @@ public static class HypnotistEvents
             mods.Do(x => x.Hysteria());
         }
     }
+
+    [RegisterEvent]
+    public static void HypnotistDeathHandler(PlayerDeathEvent @event)
+    {
+        if (@event.Player.Data.Role is HypnotistRole hypno)
+        {
+            ModifierUtils.GetPlayersWithModifier<HypnotisedModifier>(x => x.Hypnotist == hypno)
+                         .Do(x => x.RemoveModifier<HypnotisedModifier>());
+        }
+    }
+
+    /* whenever the event gets changed to having the character not being null
+    [RegisterEvent]
+    public static void HypnotistDisconnectHandler(PlayerLeaveEvent @event)
+    {
+        if (@event.ClientData.Character.Data.Role is HypnotistRole hypno)
+        {
+            ModifierUtils.GetPlayersWithModifier<HypnotisedModifier>(x => x.Hypnotist == hypno)
+                         .Do(x => x.RemoveModifier<HypnotisedModifier>());
+        }
+    }*/
 }
