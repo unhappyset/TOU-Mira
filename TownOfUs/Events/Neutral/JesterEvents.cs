ebaead62a20e58f0d5461a6a7af07240c003be15
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-08-30 17:21:06 -0500

    Refactor Ghost Murders

--- TownOfUs/Events/Neutral/JesterEvents.cs
+++ TownOfUs/Events/Neutral/JesterEvents.cs
@@ -49,7 +49,6 @@ public static class JesterEvents
                 notif1.transform.localPosition = new Vector3(0f, 1f, -20f);
                 if (OptionGroupSingleton<JesterOptions>.Instance.JestWin is JestWinOptions.Haunts)
                 {
-                    PlayerControl.LocalPlayer.RpcAddModifier<IndirectAttackerModifier>(true);
                     CustomButtonSingleton<JesterHauntButton>.Instance.SetActive(true, jester);
                     DeathHandlerModifier.RpcUpdateDeathHandler(PlayerControl.LocalPlayer, "null", -1, DeathHandlerOverride.SetTrue, lockInfo: DeathHandlerOverride.SetTrue);
                     var notif2 = Helpers.CreateAndShowNotification(
