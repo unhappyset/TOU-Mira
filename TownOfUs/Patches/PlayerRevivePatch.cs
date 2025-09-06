497fa4ed87bf499c52d19f2a07f94746e0ce1d78
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-09-06 13:14:51 -0500

    Better ambusher checks

--- TownOfUs/Patches/PlayerRevivePatch.cs
+++ TownOfUs/Patches/PlayerRevivePatch.cs
@@ -7,7 +7,6 @@ namespace TownOfUs.Patches;
 [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Revive))]
 public static class PlayerRevivePatch
 {
-
     public static void Postfix(PlayerControl __instance)
     {
         var reviveEvent = new PlayerReviveEvent(__instance);
