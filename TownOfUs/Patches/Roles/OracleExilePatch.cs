076b8e01e50eeecee74a3a13b5793b933b29302c
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2025-10-08 20:12:14 -0300

    (actually) fix broken LI exile text

--- TownOfUs/Patches/Roles/OracleExilePatch.cs
+++ TownOfUs/Patches/Roles/OracleExilePatch.cs
@@ -3,6 +3,7 @@ using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using MiraAPI.Modifiers;
 using TownOfUs.Modifiers.Crewmate;
 using TownOfUs.Utilities.Appearances;
+using static ExileController;
 using Object = Il2CppSystem.Object;
 
 namespace TownOfUs.Patches.Roles;
@@ -10,31 +11,36 @@ namespace TownOfUs.Patches.Roles;
 [HarmonyPatch]
 public static class OracleExilePatch
 {
-    [HarmonyPatch(typeof(TranslationController), nameof(TranslationController.GetString), typeof(StringNames),
-        typeof(Il2CppReferenceArray<Object>))]
-    [HarmonyPostfix]
-    public static void TranslationControllerGetStringPostfix(ref string __result, [HarmonyArgument(0)] StringNames name)
+    [HarmonyPatch(typeof(ExileController), nameof(ExileController.HandleText))]
+    [HarmonyPatch(typeof(AirshipExileController), nameof(AirshipExileController.HandleText))]
+    [HarmonyPrefix]
+    public static void TranslationControllerGetStringPostfix(ExileController __instance)
     {
-        if (ExileController.Instance == null)
+        if (__instance.initData.networkedPlayer != null)
         {
             return;
         }
 
-        if (ExileController.Instance.initData.networkedPlayer != null)
-        {
-            return;
-        }
+        var blessedPlayers = new List<string>();
 
         foreach (var mod in ModifierUtils.GetActiveModifiers<OracleBlessedModifier>())
         {
             if (!mod.SavedFromExile)
-            {
                 continue;
-            }
 
             mod.SavedFromExile = false;
+            blessedPlayers.Add(mod.Player.GetDefaultAppearance().PlayerName);
+        }
 
-            __result = $"{mod.Player.GetDefaultAppearance().PlayerName} was blessed by an Oracle!";
+        if (blessedPlayers.Count == 1)
+        {
+            __instance.completeString = $"{blessedPlayers[0]} was blessed by an Oracle!";
+        }
+        else if (blessedPlayers.Count > 1)
+        {
+            var allButLast = string.Join(", ", blessedPlayers.Take(blessedPlayers.Count - 1));
+            var last = blessedPlayers[^1];
+            __instance.completeString = $"{allButLast}, and {last} were blessed by an Oracle!";
         }
     }
 }
